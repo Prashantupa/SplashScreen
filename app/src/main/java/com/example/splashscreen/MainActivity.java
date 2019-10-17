14af40df05701b0c0653255f5cf874ae4ea471be
Prashant Upadhyay <[prashantipa25@gmail.com]>
2019-10-17 16:23:24 +0530

    SplashScreen

---
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.splashscreen;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    public static int SPLASH_TIME_OUT = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
